43e089d3c630052e93b641e1cb1531f9d6183a29
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-12-23 01:08:22 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {useState,useEffect}from'react';
 import Counter from './componets/Counter.js';
 function App() {
- return(
+  const[buttonName, setButtonName]= useState('클릭');
+  const clickButton = ()=>{
+    setButtonName('click');
+  };
+   return(
    <div class="App">
    <h1>Kossi coder</h1>
+  <Counter click="click1"/>
+  <Counter click={buttonName}/>
   <Counter/>
-  <Counter/>
-  <Counter/>
+  <button onClick={clickButton}>Click</button>
   </div>
  );
 }
--- src/componets/Counter.js
+++ src/componets/Counter.js
@@ -1,12 +1,15 @@
 import React, {useState,useEffect}from'react';
 
-const Counter=()=>{
+const Counter=(props)=>{
     const [count, setCount]= useState(0);
     const increment=()=>{
       setCount(count+1);
     };
+    const clickString = props.click || 'Click';
     return (
-          <button onClick={increment}>click {count}</button>
+         <p>
+              <button onClick={increment}>{clickString} {count}</button>
+              </p>
     );
 };
 
