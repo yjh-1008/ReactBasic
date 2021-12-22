49e9b6525f46b370e2cc5715a05f792616ade5fc
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-12-23 01:16:24 +0900

    render

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { render } from '@testing-library/react';
 import React, {useState,useEffect}from'react';
 import Counter from './componets/Counter.js';
 function App() {
   const[buttonName, setButtonName]= useState('클릭');
-  const clickButton = ()=>{
-    setButtonName('click');
+  const [condition, setCondition]= useState(false);
+  const toggle=()=>{
+    setCondition(!condition);
   };
+  useEffect(()=>{
+    console.log(condition);
+  },[condition]);
+  const renderCondition=(condition
+  ?'TRUE'
+  :'FALSE');
    return(
    <div class="App">
    <h1>Kossi coder</h1>
   <Counter click="click1"/>
   <Counter click={buttonName}/>
   <Counter/>
-  <button onClick={clickButton}>Click</button>
+  <div>  {renderCondition}</div>
+  <button onClick={toggle}>toggle</button>
   </div>
  );
 }
